c91c28146e622e51689cd4553d54afb3472aa858
dc-rubiano-rojas <dcrubiano02@gmail.com>
2022-02-24 19:19:33 -0500

    :tada: First commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ionic-auth-file-upload',
+    appId: '1:14889553814:web:f2ec63a8eb03c6be5957b7',
+    storageBucket: 'ionic-auth-file-upload.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCiKJBB-JWKDhqy-_92YiSmDsUzxPnV5x4',
+    authDomain: 'ionic-auth-file-upload.firebaseapp.com',
+    messagingSenderId: '14889553814',
+  },
   production: false
 };
 
